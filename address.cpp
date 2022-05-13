18afc3c501a5550d99b8253f668731d8bb0d6673
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-05-13 13:46:24 +0000

    in progress

---
+++ address.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2022 Sarah All rights reserved.
+//
+// Created by: Sarah
+// Created on: May, 13th, 2022
+// This program asks the user several questions regarding their address. It
+// then formats it and displays it to the user.
+#include <iostream>
+
+def formatAddress()
+
+int main() {
+    // declare variables
+    std::string userName, questionAtp, aptNumUser, userNumStreet;
+    std::string userNameStreet, userCity, userProvince, userPostal;
+    std::string
+	
+	std::cout << "This program formats a Canadian mailing addres";
+	std:cout << std::endl;
+	
+	std::cout << "Enter your full name: ";
+	std::cin >> userName;
+    std::cout << "Do you live in an apartment (y/n): ";
+    std::cin >> questionAtp;
+    
+    if (questionAtp == "y") {
+        std::cout << "Enter your apartment number: ";
+    // get more user input
+    std::cout << "Enter your street number: ";
+    std::cin << userNumStreet;
+    std::cout << "Enter you street name AND "
+                 "type (Main St., Flower Cres., etc): ";
+    std::cout <<"Enter your city: ";
+    std::cin >> userCity
+    
+    std::cout << "Enter your province (as an abbreviation, i.e ON, AB, etc): ";
+    std::cin >> userProvince
+    
+    std::cout << "Enter your postal code (A1B 2C3): ";
+    std::cin >> userPostal;
+    }
+    
+    if (aptNumUser != "") {
+        
+        
+    }
+    
+}
