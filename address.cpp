11b24b508d764c4235a670e56fb01990c20f44ea
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-05-14 04:42:53 +0000

    Made a program that formats all user input into a Canadian mailing address format.

--- address.cpp
+++ address.cpp
@@ -5,55 +5,85 @@
 // This program asks the user several questions regarding their address. It
 // then formats it and displays it to the user.
 #include <iostream>
+#include <iomanip>
+#include <string>
 
-def formatAddress(std::string name, std::string question, std::string 
-                 numStreet, std::string nameStreet, std::string city, 
-                 std::string province, std::string postal,
-                 std::string aptNum == "") {
-    
-    std:: string formatAddress;
-    // returns full name
-    
-    
+std::string formatAddress(std::string name, std::string question, std::string
+                          numStreet, std::string nameStreet, std::string city,
+                          std::string province, std::string postal,
+                          std::string aptNum = "") {
+    // declare string
+    std::string address = name;
+    // handles the case if user lives in an apartment or not, then it
+    // formats it.
+    if (aptNum != "") {
+        address = address + "\n" + aptNum + "-" + numStreet + " " + nameStreet +
+                  "\n" + city + " " + province + " " + postal;
+    } else {
+        address = address + "\n" + numStreet + " " + nameStreet + " \n"
+                  + city + " " + province + " " + postal;
+    }
 
-
-
+    // return function
+    return address;
+}
 
 
 int main() {
-    // declare variables
+    // declare variable & string
     std::string userName, questionAtp, aptNumUser, userNumStreet;
     std::string userNameStreet, userCity, userProvince, userPostal;
-    std::string
-	
-	std::cout << "This program formats a Canadian mailing addres";
-	std:cout << std::endl;
-	
-	std::cout << "Enter your full name: ";
-	std::cin >> userName;
+    std::string fullAddress;
+
+    aptNumUser == "";
+
+    std::cout << "This program formats a Canadian mailing address";
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    // gets user input
+    std::cout << "Enter your full name: ";
+    getline(std::cin, userName);
     std::cout << "Do you live in an apartment (y/n): ";
-    std::cin >> questionAtp;
-    
+    getline(std::cin, questionAtp);
+
+    // checks to see if user lives in an apartment and gathers user input to
+    // format it to an address.
     if (questionAtp == "y") {
         std::cout << "Enter your apartment number: ";
-    // get more user input
+        getline(std::cin, aptNumUser);
+    }
+
     std::cout << "Enter your street number: ";
-    std::cin << userNumStreet;
+    getline(std::cin, userNumStreet);
+
     std::cout << "Enter you street name AND "
                  "type (Main St., Flower Cres., etc): ";
+    getline(std::cin, userNameStreet);
+
     std::cout << "Enter your city: ";
-    std::cin >> userCity
-    
+    getline(std::cin, userCity);
+
     std::cout << "Enter your province (as an abbreviation, i.e ON, AB, etc): ";
-    std::cin >> userProvince
-    
+    getline(std::cin, userProvince);
+
     std::cout << "Enter your postal code (A1B 2C3): ";
-    std::cin >> userPostal;
-    }
-    
+    getline(std::cin, userPostal);
+
+    // assigns a variable to function call, giving it a returned value.
     if (aptNumUser != "") {
-        
-        
+        fullAddress = formatAddress(userName, questionAtp, userNumStreet,
+                                    userNameStreet, userCity, userProvince,
+                                    userPostal, aptNumUser);
+
+    } else {
+        fullAddress = formatAddress(userName, questionAtp, userNumStreet,
+                                    userNameStreet, userCity, userProvince,
+                                    userPostal);
     }
-    
+
+    // displays the result to user, properly formatted.
+    std::cout << std::endl;
+    std::cout << "Your Canadian mail address is: " << std::endl;
+    std::cout << fullAddress << std::endl;
 }
